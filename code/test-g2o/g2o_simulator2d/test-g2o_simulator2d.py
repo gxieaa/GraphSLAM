3a1dedf25eae0af382a96208d3459af314701336
Franco <francocurotto@gmail.com>
2015-10-02 08:59:59 -0300

    asdf

--- code/test-g2o/g2o_simulator2d/test-g2o_simulator2d.py
+++ code/test-g2o/g2o_simulator2d/test-g2o_simulator2d.py
@@ -4,15 +4,15 @@ import subprocess
 
 def main():
     # variables
-    #binPath = "../../my-scripts/my-simulator/build/"
-    binPath = "../../g2o-master/bin/"
+    binPath = "../../my-scripts/my-simulator/build/"
+    #binPath = "../../g2o-master/bin/"
     resPath = "res.g2o"
     
     start_time = time.time()
     subprocess.call([binPath+"./my_simulator", 
     "-hasOdom", "-hasPointSensor",
     "-nlandmarks", "500",
-     "-simSteps", "500",
+    "-simSteps", "500",
     resPath])
     elapsed_time = time.time() - start_time
     print "Total time: " + str(elapsed_time) + " [s]"
